13180d6ead643259b982fe11ac881d229cd5c155
Brian Marquis <rbmarquis.dev@gmail.com>
2024-04-27 21:03:41 -0500

    add header and button to Main

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,3 +1,23 @@
+import { Button, Flex, Heading } from "@chakra-ui/react";
+
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
+        <Heading
+          color={"blue.400"}
+          display={["none", "block", "block", "block", "block"]}
+        >
+          Expense Tracker
+        </Heading>
+        <Flex alignItems={"center"}>
+          <Button bg={"blue.300"} color={"black"} ml={"4"}>
+            Add new transaction
+          </Button>
+        </Flex>
+      </Flex>
+      {/* // overview */}
+      <Flex>{/* expense-view */}</Flex>
+    </Flex>
+  );
 }
