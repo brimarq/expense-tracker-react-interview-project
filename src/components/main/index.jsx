f5dc26db45f88cb15cc5e646b8bc09fde4adcc28
Brian Marquis <rbmarquis.dev@gmail.com>
2024-05-11 18:04:51 -0500

    wire up data to Summary component & chart view

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,9 +1,32 @@
 import { Button, Flex, Heading, useDisclosure } from "@chakra-ui/react";
 import Summary from "../summary";
 import ExpenseView from "../expense-view";
+import { useContext, useEffect } from "react";
+import { GlobalContext } from "../../context";
 
 export default function Main() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    totalExpense,
+    allTransactions,
+    setTotalExpense,
+    totalIncome,
+    setTotalIncome,
+  } = useContext(GlobalContext);
+
+  useEffect(() => {
+    let income = 0;
+    let expense = 0;
+
+    allTransactions.forEach((item) => {
+      item.type === "income"
+        ? (income = income + parseFloat(item.amount))
+        : (expense = expense + parseFloat(item.amount));
+    });
+
+    setTotalExpense(expense);
+    setTotalIncome(income);
+  }, [allTransactions]);
 
   return (
     <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
@@ -20,7 +43,12 @@ export default function Main() {
           </Button>
         </Flex>
       </Flex>
-      <Summary isOpen={isOpen} onClose={onClose} />
+      <Summary
+        totalExpense={totalExpense}
+        totalIncome={totalIncome}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
       <Flex
         w={"full"}
         alignItems={"flex-start"}
